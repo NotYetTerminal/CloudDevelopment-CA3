8df9ab5ab0dfc67701a18592e9bf430ba7716bd3
Gabor Major <61381495+NotYetTerminal@users.noreply.github.com>
2023-11-24 22:09:41 +0000

    Added new functions

--- webapp/app.py
+++ webapp/app.py
@@ -2,54 +2,55 @@ from flask import Flask, render_template, request  # from module import Class.
 
 
 import os
-
+import DBcm
 import hfpy_utils
 import swim_utils
+
+
+config = {
+    "user": "swimuser",
+    "password": "swimuserpasswd",
+    "database": "SwimclubDB",
+    "host": "localhost",
+}
 
 
 app = Flask(__name__)
 
 
 @app.get("/")
+@app.get("/selectdate")
+def select_swim_date():
+    convert2hundreths(3)
+    dates: list = [(x.split(" ")[0], x) for x in query_dates()]
+
+    return render_template(
+        "select.html",
+        title="Select a swim session",
+        label_text="Please select a chosen_date:",
+        data_list=dates,
+        action="/getswimmers",
+    )
+
+
 @app.get("/getswimmers")
 def get_swimmers_names():
-    files = os.listdir(swim_utils.FOLDER)
-    names = set()
-
-    for swimmer in files:
-        names.add(swim_utils.get_swimmers_data(swimmer)[0])
-
-    events = [(x, x) for x in sorted(names)]
+    chosen_date: str = request.form["select_data"]
+    swimmers: list = query_swimmers(chosen_date)
 
     return render_template(
         "select.html",
         title="Select a swimmer to chart",
         label_text="Choose a swimmer:",
-        data_list=events,
+        data_list=swimmers,
         action="/displayevents",
     )
 
 
 @app.post("/displayevents")
 def get_swimmer_events():
-    chosen_name = request.form["swimmer"]
-    files = os.listdir(swim_utils.FOLDER)
-    events = []
-
-    swimmer_age = 0
-
-    for swimmer in files:
-        (
-            name,
-            age,
-            distance,
-            stroke,
-            the_times,
-            converts,
-            the_average,
-        ) = swim_utils.get_swimmers_data(swimmer)
-        if name == chosen_name:
-            events.append((f"{swimmer}", f"{distance}-{stroke}"))
+    date, swimmer_id = request.form["select_data"].split("-")
+    events: list = query_events(date, int(swimmer_id))
 
     return render_template(
         "select.html",
@@ -62,24 +63,20 @@ def get_swimmer_events():
 
 @app.post("/chart")
 def display_chart():
-    (
-        name,
-        age,
-        distance,
-        stroke,
-        the_times,
-        converts,
-        the_average,
-    ) = swim_utils.get_swimmers_data(request.form["swimmer"])
+    date, swimmer_id, event_id = request.form["select_data"].split("-")
+    times: list = query_events(date, int(swimmer_id), int(event_id))
+
+    converts: list = [convert2hundreths(x.removeprefix("00:")) for x in times]
+    average: str = build_time_string(mean(converts))
 
     converts.reverse()
-    the_times.reverse()
+    times.reverse()
 
     the_title = f"{name} (Under {age}) {distance} {stroke}"
     from_max = max(converts) + 50
     the_converts = [hfpy_utils.convert2range(n, 0, from_max, 0, 350) for n in converts]
 
-    the_data = zip(the_converts, the_times)
+    the_data = zip(the_converts, times)
 
     return render_template(
         "chart.html",
