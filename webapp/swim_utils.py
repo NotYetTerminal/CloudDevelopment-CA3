7a1b82e6bb069fbd09403de2d244f5a9590bebdb
Gabor Major <61381495+NotYetTerminal@users.noreply.github.com>
2023-11-24 22:09:25 +0000

    Added queries

--- webapp/swim_utils.py
+++ webapp/swim_utils.py
@@ -40,3 +40,58 @@ def get_swimmers_data(filename):
     average = build_time_string(mean(converts))
 
     return name, age, distance, stroke, times, converts, average
+
+
+def query_dates() -> list:
+    SQL: str = f""" SELECT DISTINCT ts FROM times;"""
+    with DBcm.UseDatabase(config) as db:
+        db.execute(SQL)
+        data: list = db.fetchall()
+
+    return data
+
+
+def query_swimmers(date: str) -> list:
+    SQL: str = f""" SELECT swimmer_id, name, age
+                    FROM swimmers
+                    ORDER BY name
+                    INNER JOIN times ON times.swimmer_id = swimmers.swimmer_id
+                    WHERE times.ts = {date};"""
+
+    with DBcm.UseDatabase(config) as db:
+        db.execute(SQL)
+        data: list = db.fetchall()
+
+    return [[date + "-" + str(row[0]), row[1] + "-" + str(row[2])] for row in data]
+
+
+def query_events(date: str, swimmer_id: int) -> list:
+    SQL: str = f""" SELECT event_id, distance, stroke
+                    FROM events
+                    ORDER BY distance
+                    INNER JOIN times ON times.event_id = events.event_id
+                    INNER JOIN swimmers ON swimmers.swimmer_id = times.swimmer_id
+                    WHERE times.ts = {date} AND times.swimmer_id = {swimmer_id};"""
+
+    with DBcm.UseDatabase(config) as db:
+        db.execute(SQL)
+        data: list = db.fetchall()
+
+    return [
+        [date + "-" + str(swimmer_id) + "-" + str(row[0]), row[1] + "-" + str(row[2])]
+        for row in data
+    ]
+
+
+def query_times(date: str, swimmer_id: int, event_id: int) -> list:
+    SQL: str = f""" SELECT time
+                    FROM times
+                    INNER JOIN events ON events.event_id = times.event_id 
+                    INNER JOIN swimmers ON swimmers.swimmer_id = times.swimmer_id
+                    WHERE times.ts = {date} AND times.swimmer_id = {swimmer_id} AND times.event_id = {event_id};"""
+
+    with DBcm.UseDatabase(config) as db:
+        db.execute(SQL)
+        data: list = db.fetchall()
+
+    return data
